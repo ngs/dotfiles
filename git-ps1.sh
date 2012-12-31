b111eab3463fbedcbe857e5ff04d46bb112f0015
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-12-31 13:34:39 +0900

    GIT PS1

--- git-ps1.sh
+++ git-ps1.sh
@@ -93,17 +93,17 @@ UserAndHost="\u@\h"
 # This PS1 snippet was adopted from code for MAC/BSD I saw from: http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
 # I tweaked it to work on UBUNTU 11.04 & 11.10 plus made it mo' better
 
-export PS1=$White$UserAndHost$Color_Off'$(git branch &>/dev/null;\
+export PS1=$UserAndHost$Color_Off:'$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
 echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
 if [ "$?" -eq "0" ]; then \
 # @4 - Clean repository - nothing to commit
-echo "'$Green'"$(__git_ps1 " (%s)"); \
+echo "'$Green'"$(__git_ps1 "%s");\
 else \
 # @5 - Changes to working tree
-echo "'$IRed'"$(__git_ps1 " {%s}"); \
-fi) '$BYellow$PathShort$Color_Off' > "; \
+echo "'$IRed'"$(__git_ps1 "%s!");\
+fi)'$BYellow$PathShort$Color_Off'"; \
 else \
 # @2 - Prompt when not in GIT repo
-echo " '$Yellow$PathShort$Color_Off' > "; \
-fi)'
+echo "'$Yellow$PathShort$Color_Off'"; \
+fi)$ '
