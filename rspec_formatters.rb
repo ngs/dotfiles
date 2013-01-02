d49ee4c4c2979f3d81783eefa00f950724a786d8
Atsushi NAGASE <atsn.ngs@gmail.com>
2013-01-03 01:39:41 +0900

    LOAD_PATH

--- rspec_formatters.rb
+++ rspec_formatters.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p194/gems/nyan-cat-formatter-0.2.0/lib')
+$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p194/gems/rb-blink1-0.0.5/lib')
+$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p194/gems/rb-blink1-0.0.5/ext')
 $LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p194/gems/blink1-formatter-0.0.4/lib')
