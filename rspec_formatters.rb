08dadb5811904d76f31be38cc025465e10fae0cf
Atsushi NAGASE <atsn.ngs@gmail.com>
2013-01-24 07:46:12 +0900

    ruby p362 to p374

--- rspec_formatters.rb
+++ rspec_formatters.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p362/gems/nyan-cat-formatter-0.2.0/lib')
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p362/gems/rb-blink1-0.0.5/lib')
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p362/gems/rb-blink1-0.0.5/ext')
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p362/gems/blink1-formatter-0.0.4/lib')
+$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/nyan-cat-formatter-0.2.0/lib')
+$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/rb-blink1-0.0.5/lib')
+$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/rb-blink1-0.0.5/ext')
+$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/blink1-formatter-0.0.4/lib')
