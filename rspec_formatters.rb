d3a9e44794607cc92fa7755970378fead1da0960
Atsushi NAGASE <atsn.ngs@gmail.com>
2013-02-18 16:39:51 +0900

    Update LOAD_PATH

--- rspec_formatters.rb
+++ rspec_formatters.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/nyan-cat-formatter-0.2.0/lib')
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/rb-blink1-0.0.5/lib')
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/rb-blink1-0.0.5/ext')
-$LOAD_PATH.unshift('~/.rvm/gems/ruby-1.9.3-p374/gems/blink1-formatter-0.0.4/lib')
+GEMS = "#{ENV['RBENV_ROOT']}/versions/1.9.3-p374/lib/ruby/gems/1.9.1/gems"
+
+$LOAD_PATH.unshift("#{GEMS}/nyan-cat-formatter-0.4.0/lib")
+$LOAD_PATH.unshift("#{GEMS}/rb-blink1-0.0.6/lib")
+$LOAD_PATH.unshift("#{GEMS}/rb-blink1-0.0.6/ext")
+$LOAD_PATH.unshift("#{GEMS}/blink1-formatter-0.0.4/lib")
+
