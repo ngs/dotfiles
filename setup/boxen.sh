b1d912e6ee3411046b17adbf119a295ebb56ce06
Atsushi Nagase <a@ngs.io>
2014-05-07 08:11:59 +0900

    symlink /opt/boxen/rbenv

--- setup/boxen.sh
+++ setup/boxen.sh
@@ -1,4 +1,4 @@
 DOTFILES=$(cd $(dirname $0)/../ && pwd)
+[ ! -L /opt/rbenv ] && ln -s /opt/boxen/rbenv /opt/rbenv
 brew bundle "${DOTFILES}/Brewfiles/boxen"
 brew bundle "${DOTFILES}/Brewfiles/fonts"
-
