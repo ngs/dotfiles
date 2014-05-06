db6c524a6504399b6a2279352aaa51daf5bb1300
Atsushi Nagase <a@ngs.io>
2014-05-06 12:11:51 +0900

    git config and zsh site-functions

--- env.sh
+++ env.sh
@@ -10,17 +10,26 @@ loadenv() {
       source $f
     fi
   done
-  if [ $BASH ]; then
-    for f in $1/*.bash ; do
-      if [ -f $f ] ; then
-        source $f
-      fi
-    done
-  fi
 }
 [ -f $ENVD/secret/env.sh   ] && loadenv $ENVD/secret
 [ $UNAME    = 'Darwin'     ] && loadenv $ENVD/darwin
 [ $UNAME    = 'Linux'      ] && loadenv $ENVD/linux
 [ $HOSTNAME = 'oglethorpe' ] && loadenv $ENVD/dreamhost
 loadenv $ENVD
+
+if [ $BASH ]; then
+  for f in $1/*.bash ; do
+    if [ -f $f ] ; then
+      source $f
+    fi
+  done
+fi
+if false && [ $ZSH_VERSION ] ; then
+  for f in $1/*.zsh ; do
+    if [ -f $f ] ; then
+      source $f
+    fi
+  done
+fi
+
 /bin/sh -c "cd ${DOTFILES}; git status --porcelain"
