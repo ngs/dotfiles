fb1d4100fca33b5d771708ca8ce655bab978a481
Atsushi Nagase <a@ngs.io>
2014-10-11 11:35:59 +0900

    update java.sh

--- env.d/java.sh
+++ env.d/java.sh
@@ -2,7 +2,6 @@ if [ -f /usr/libexec/java_home ]; then
   export JAVA_HOME="$(/usr/libexec/java_home)"
   export PATH=${JAVA_HOME}/bin:${PATH}:
 fi
-export _JAVA_OPTIONS=-Duser.language=en
 CLASSPATH=${CLASSPATH}
 jars=( $(find "${HOME}/dotfiles/lib/java" -name "*.jar") )
 for jar in ${jars}; do
