1100e532e1d4f5643d0fe57316c2a365275b5183
Atsushi Nagase <a@ngs.io>
2015-06-01 07:52:04 +0900

    Homebrew bundle wip

--- env.d/node.sh
+++ env.d/node.sh
@@ -2,3 +2,8 @@ if [ -d $HOME/.nodenv/bin ]; then
   export PATH=$HOME/.nodenv/bin:$PATH
   eval "$(nodenv init -)"
 fi
+
+if [ -d $HOME/.nodenv/shims ]; then
+  export PATH=$HOME/.nodenv/shims:$PATH
+  eval "$(nodenv init -)"
+fi
--- setup/keyremap4macbook.sh
+++ setup/keyremap4macbook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 $cli set repeat.wait 10
 $cli set repeat.initial_wait 200
