4f47d31e8fb0629d5b74b7ea4b437fbcba9b699f
Atsushi Nagase <a@ngs.io>
2015-07-02 11:50:00 +0900

    Use boot2docker shellinit

--- env.d/darwin/docker.sh
+++ env.d/darwin/docker.sh
@@ -1,5 +1,2 @@
-export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-
+eval "$(boot2docker shellinit 2>&/dev/null)"
 
