d815adc2857d8d565ffef799b2a953095b7d6488
Atsushi Nagase <a@ngs.io>
2015-09-24 11:03:21 +0900

    Install terminal-notifier

--- setup.sh
+++ setup.sh
@@ -75,5 +75,5 @@ vim +PluginInstall +qall > /dev/null 2>&1
 
 [ -L $DOTFILES/rc.d/vim/bundle/closetag ] || ln -s $DOTFILES/rc.d/vim/bundle/closetag.vim $DOTFILES/rc.d/vim/bundle/closetag
 
-pip install --upgrade -r requirements.tx
+pip install --upgrade -r requirements.txt
 
