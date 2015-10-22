7e0ff03f6acf9234174abf738cc8f65961c7baff
Atsushi Nagase <a@ngs.io>
2015-10-23 06:57:38 +0900

    sketchpreset

--- setup/darwin.sh
+++ setup/darwin.sh
@@ -19,3 +19,9 @@ apm install --packages-file Atomfile
 apm upgrade --confirm=false
 apm list -bi > Atomfile
 
+##
+## Import Sketch presets
+##
+for f in $DOTFILES/ide/sketch/*; do
+  cat $f > ~/Library/Containers/com.bohemiancoding.sketch3/Data/Library/Application\ Support/com.bohemiancoding.sketch3/$(basename "$f")
+done
