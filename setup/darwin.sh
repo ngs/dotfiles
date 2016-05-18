7763cebc7c3e849018347eae2c7768dc1981b12e
Atsushi Nagase <a@ngs.io>
2016-05-19 05:54:23 +0900

    Import gpg keys

--- setup/darwin.sh
+++ setup/darwin.sh
@@ -1,10 +1,18 @@
 DOTFILES=$(cd $(dirname $0)/.. && pwd)
+
 ##
 [ `which brew` ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
 brew bundle --file="${DOTFILES}/Brewfiles/default"
 # brew bundle --file="${DOTFILES}/Brewfiles/kaizen"
 brew bundle --file="${DOTFILES}/Brewfiles/fonts"
+
+## Import gpg key
+if [ -d ~/Dropbox/Credentials/gpg ]; then
+  cd ~/Dropbox/Credentials/gpg
+  ./import.sh
+fi
+
 ##
 ## Import iTerm 2 Themes
 ##
