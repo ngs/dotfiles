560782fdff30811dd2503c8d6c266a69226ff611
Atsushi Nagase <a@ngs.io>
2016-12-24 00:41:29 +0900

    brew update

--- setup/darwin/homebrew.sh
+++ setup/darwin/homebrew.sh
@@ -2,6 +2,7 @@
 DOTFILES=$(cd $(dirname $0)/../.. && pwd)
 ##
 [ `which brew` ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
 brew tap Homebrew/bundle
 brew bundle --file="${DOTFILES}/Brewfiles/default"
 brew bundle --file="${DOTFILES}/Brewfiles/fonts"
