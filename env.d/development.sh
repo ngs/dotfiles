5b1c1be21eda3e7435b6fb838871409b45421503
Atsushi Nagase <a@ngs.io>
2017-01-09 00:48:39 +0900

    Do not reference inexistent directories

--- env.d/development.sh
+++ env.d/development.sh
@@ -1,11 +1,27 @@
 LD_LIBRARY_PATH=/usr/lib
-[ -d /usr/lib64 ] && LD_LIBRARY_PATH="/usr/lib64:${LD_LIBRARY_PATH}"
-[ -d /usr/local/lib ] && LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
-[ -d /usr/local/lib64 ] && LD_LIBRARY_PATH="/usr/local/lib64:${LD_LIBRARY_PATH}"
-[ -d $HOME/local/lib ] && LD_LIBRARY_PATH="${HOME}/local/lib:${LD_LIBRARY_PATH}"
-LDFLAGS+=" -L/usr/local/lib64 -L/usr/lib -L/usr/lib64 -L/usr/local/lib -L$HOME/local/lib "
-CPPFLAGS+=" -I/usr/local/include -I$HOME/local/include -fPIC "
-CFLAGS+=" -I/usr/local/include -I$HOME/local/include -fPIC "
+if [ -d /usr/lib64 ]; then
+  LD_LIBRARY_PATH="/usr/lib64:${LD_LIBRARY_PATH}"
+  LDFLAGS="-L/usr/lib64 ${LDFLAGS}"
+fi
+if [ -d /usr/local/lib ]; then
+  LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
+  LDFLAGS="-L/usr/local/lib ${LDFLAGS}"
+fi
+if [ -d /usr/local/lib64 ]; then
+  LD_LIBRARY_PATH="/usr/local/lib64:${LD_LIBRARY_PATH}"
+  LDFLAGS="-L/usr/local/lib64 ${LDFLAGS}"
+fi
+if [ -d $HOME/local/lib ]; then
+  LD_LIBRARY_PATH="${HOME}/local/lib:${LD_LIBRARY_PATH}"
+fi
+CPPFLAGS="-fPIC "
+if [ -d /usr/local/include ]; then
+  CPPFLAGS+="-I/usr/local/include ${CPPFLAGS}"
+fi
+if [ -d "${HOME}/local/include" ]; then
+  CPPFLAGS+="-I${HOME}/local/include ${CPPFLAGS}"
+fi
+CFLAGS+=$CPPFLAGS
 #export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:/usr/local/lib:$HOME/local/lib
 #export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib:$HOME/local/lib
 
