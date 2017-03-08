4c84229cfae74c543c314f0c3f65b78bf45772e0
Atsushi Nagase <a@ngs.io>
2017-03-08 23:57:06 +0900

    Fix setup/theme.sh

--- setup/darwin/theme.sh
+++ setup/darwin/theme.sh
@@ -2,10 +2,13 @@
 DOTFILES=$(cd $(dirname $0)/../.. && pwd)
 ##
 ## Import iTerm 2 Themes
-##
-for f in $DOTFILES/themes/iterm2/*; do
-  THEME=$(basename "$f")
-  defaults write -app '/usr/local/Caskroom/iterm2/2.0/iTerm.app' 'Custom Color Presets' -dict-add "$THEME" "$(cat "$f")"
+#
+
+for app in $(find /usr/local/Caskroom/iterm2 -name 'iTerm.app'); do
+  for f in $DOTFILES/themes/iterm2/*; do
+    THEME=$(basename "$f")
+    defaults write -app $app 'Custom Color Presets' -dict-add "$THEME" "$(cat "$f")"
+  done
 done
 
 [ -e ~/Documents/tomorrow-theme ] || /bin/sh -c 'git clone git@github.com:chriskempson/tomorrow-theme.git ~/Documents/tomorrow-theme'
@@ -15,5 +18,3 @@ for f in ~/Documents/tomorrow-theme/OS\ X\ Terminal/*.terminal; do
   THEME=${THEME%.terminal}
   defaults write com.apple.Terminal "Window Settings" -dict-add "${THEME}" "$(cat "$f")"
 done
-
-
