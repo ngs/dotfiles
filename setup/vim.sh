95e860784dde25c536515e5f8f71c5bd863aceda
Atsushi Nagase <a@ngs.io>
2017-03-08 12:20:37 +0900

    Update vim stuff

--- setup/vim.sh
+++ setup/vim.sh
@@ -2,5 +2,5 @@
 
 DOTFILES=$(cd $(dirname $0)/.. && pwd)
 
-vim +PluginInstall +GoInstallBinaries +qall > /dev/null 2>&1
+vim +PluginInstall! +PluginClean! +GoInstallBinaries +qall > /dev/null 2>&1
 [ -L $DOTFILES/rc.d/vim/bundle/closetag ] || ln -s $DOTFILES/rc.d/vim/bundle/closetag.vim $DOTFILES/rc.d/vim/bundle/closetag
