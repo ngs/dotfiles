24bc53e45c7e321a66b352befe8915fdc5cfad2b
Atsushi Nagase <a@ngs.io>
2017-05-13 03:00:02 +0900

    Perl

--- env.d/perl.sh
+++ env.d/perl.sh
@@ -1,3 +1,8 @@
 export PERL_AUTOINSTALL="--alldeps"
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=1
 
+if [ -d $HOME/.plenv ]; then
+  export PATH=$HOME/.plenv/shims:$PATH
+  eval "$(plenv init -)"
+fi
+
