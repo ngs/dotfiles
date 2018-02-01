927ac6af878274003f762f10ae66e6c6fea8d4ad
Atsushi Nagase <a@ngs.io>
2018-02-02 00:34:00 +0900

    Remove js related bash functions

--- env.d/functions.sh
+++ env.d/functions.sh
@@ -33,28 +33,6 @@ MYMETA.yml
     git push origin master
 }
 
-closure() {
-    ARG=$*
-    if [ "$ARG" = '' ]; then
-        ARG='--help'
-    fi;
-    java com.google.javascript.jscomp.CommandLineRunner $ARG 2>&1;
-}
-
-jslint() {
-  ARG=$1
-  JSLINT="${HOME}/dotfiles/tools/jslint.js"
-  if [ -f "$ARG" ]; then
-    java org.mozilla.javascript.tools.shell.Main "$JSLINT" "$ARG" 2>&1
-  else
-    echo "File not found: $ARG"
-  fi
-}
-
-js() {
-    java org.mozilla.javascript.tools.shell.Main $* 2>&1
-}
-
 jpegtopng() {
     if [ ! -z "$1" ]; then
         if [ -z "$2" ]; then
@@ -74,15 +52,6 @@ svn-mime() {
     find . -name "*.gif" | xargs svn ps svn:mime-type image/gif
     find . -name "*.jpg" | xargs svn ps svn:mime-type image/jpeg
     find . -name "*.swf" | xargs svn ps svn:mime-type application/x-shockwave-flash
-}
-
-jsdoc() {
-    ARG=$*
-    ARGCONF=''
-    if [ "$1" != '--conf' ]; then
-        ARGCONF="--conf=${HOME}/.jsdoc.conf"
-    fi
-    ${JSDOCDIR}/jsrun.sh $ARGCONF $ARG
 }
 
 standby() {
