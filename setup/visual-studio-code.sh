a522e7a41dec08879158a447ad5924515d71b572
Atsushi Nagase <a@ngs.io>
2018-03-12 18:33:08 +0900

    Install VSCode

---
+++ setup/visual-studio-code.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOTFILES=$(cd $(dirname $0)/.. && pwd)
+VSCODE_FILE="${DOTFILES}/VSCodefile"
+
+cat $VSCODE_FILE | while read -r EXT; do
+  code --install-extension $EXT
+done
+
+code --list-extensions > $VSCODE_FILE
+
+if [ -d "${HOME}/Library" ]; then
+  mkdir -p "${HOME}/Library/Application Support"
+  rm -rf "${HOME}/Library/Application Support/Code/User"
+  ln -s "${DOTFILES}/rc.d/vscode/userdata" "${HOME}/Library/Application Support/Code/User"
+fi
