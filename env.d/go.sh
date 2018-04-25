450d5a3c9e8e3f4533aef13c0f810ebeec82f7de
Atsushi Nagase <a@ngs.io>
2018-04-26 07:32:24 +0900

    Introduce goenv

--- env.d/go.sh
+++ env.d/go.sh
@@ -2,3 +2,10 @@ export GOPATH=$HOME/.go
 export PATH=$GOPATH/bin:$PATH
 export CGO_CFLAGS="-I/usr/local/include"
 export CGO_LDFLAGS="-L/usr/local/lib"
+GOENV_ROOT="$HOME/.goenv"
+
+if [ -d $GOENV_ROOT ]; then
+  export GOENV_ROOT
+  export PATH="$GOENV_ROOT/bin:$PATH"
+  eval "$(goenv init -)"
+fi
