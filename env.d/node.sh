59acecdd415507e75baffe6cb1c55e44e13b2a7e
Atsushi Nagase <a@ngs.io>
2018-08-08 00:37:10 +0900

    Install pyenv

--- env.d/node.sh
+++ env.d/node.sh
@@ -1,9 +1,13 @@
-if [ -d $HOME/.nodenv/bin ]; then
-  export PATH=$HOME/.nodenv/bin:$PATH
+if command -v nodenv 1>/dev/null 2>&1; then
   eval "$(nodenv init -)"
 fi
 
-if [ -d $HOME/.nodenv/shims ]; then
-  export PATH=$HOME/.nodenv/shims:$PATH
-  eval "$(nodenv init -)"
+if command -v npm 1>/dev/null 2>&1; then
+  NPM_ROOT=$(npm root -g)
+  # tabtab source for serverless package
+  # uninstall by removing these lines or running `tabtab uninstall serverless`
+  [[ -f $NPM_ROOT/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && source $NPM_ROOT/serverless/node_modules/tabtab/.completions/serverless.zsh
+  # tabtab source for sls package
+  # uninstall by removing these lines or running `tabtab uninstall sls`
+  [[ -f $NPM_ROOT/serverless/node_modules/tabtab/.completions/sls.zsh ]] && source $NPM_ROOT/serverless/node_modules/tabtab/.completions/sls.zsh
 fi
