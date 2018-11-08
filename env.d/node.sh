c9b098c9ccc3b1a813783642a353e1025136ac0f
Atsushi Nagase <a@ngs.io>
2018-11-08 14:55:31 +0900

    Update config

--- env.d/node.sh
+++ env.d/node.sh
@@ -11,3 +11,13 @@ if command -v npm 1>/dev/null 2>&1; then
   # uninstall by removing these lines or running `tabtab uninstall sls`
   [[ -f $NPM_ROOT/serverless/node_modules/tabtab/.completions/sls.zsh ]] && source $NPM_ROOT/serverless/node_modules/tabtab/.completions/sls.zsh
 fi
+
+
+NODE_MODULES=$(dirname $(dirname $(nodenv which npm)) )/lib/node_modules
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f $NODE_MODULES/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . $NODE_MODULES/serverless/node_modules/tabtab/.completions/serverless.zsh
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f $NODE_MODULES/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . $NODE_MODULES/serverless/node_modules/tabtab/.completions/sls.zsh
