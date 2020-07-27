5e872667fdf3fabcbae72fd6430bf3d18d790d52
Atsushi Nagase <a@ngs.io>
2020-07-27 01:33:05 +0000

    Install direnv

--- setup/codespaces/apt.sh
+++ setup/codespaces/apt.sh
@@ -3,7 +3,7 @@
 set -eux
 
 apt-get -u update
-apt-get -y install vim zsh pinentry-curses
+apt-get -y install vim zsh pinentry-curses direnv
 [ -f /usr/local/bin/pinentry ] || ln -s /usr/bin/pinentry /usr/local/bin/pinentry
 
 chsh -s /bin/zsh
