2b696eaa023201575f7bbb1cb5d7937346dcfbc1
Atsushi NAGASE <a@ngs.io>
2021-01-22 15:32:28 +0900

    Update apt.sh

--- setup.d/codespaces/apt.sh
+++ setup.d/codespaces/apt.sh
@@ -6,5 +6,4 @@ sudo apt-get -u update
 sudo apt-get -y install vim zsh pinentry-curses direnv jq
 [ -f /usr/local/bin/pinentry ] || sudo ln -s /usr/bin/pinentry /usr/local/bin/pinentry
 
-chsh -s /bin/zsh
 rm ~/.zshrc
