c5481c5b48cc2f04c3e2921a792a91006f657ed6
Atsushi NAGASE <a@ngs.io>
2021-01-23 09:57:45 +0900

    Update go.sh

--- setup.d/go.sh
+++ setup.d/go.sh
@@ -7,17 +7,20 @@ if command -v go 1>/dev/null 2>&1; then
 
 	# vscode-go dependencies
 	echo "Getting dependencies for the vscode-go plugin "
-	# via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-	go get -u -v github.com/acroca/go-symbols
-	go get -u -v github.com/cweill/gotests/...
-	go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-	go get -u -v github.com/haya14busa/goplay/cmd/goplay
-	go get -u -v github.com/mdempsky/gocode
-	go get -u -v github.com/ramya-rao-a/go-outline
-	go get -u -v github.com/rogpeppe/godef
-	go get -u -v github.com/sqs/goreturns
-	go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-	go get -u -v github.com/zmb3/gogetdoc
-	go get -u -v golang.org/x/lint/golint
-	go get -u -v golang.org/x/tools/cmd/gorename
+	# via: https://github.com/golang/vscode-go/blob/master/.github/workflows/test-long.yml#L46-L60
+	go version
+	go get github.com/acroca/go-symbols
+	go get github.com/davidrjenni/reftools/cmd/fillstruct
+	go get github.com/haya14busa/goplay/cmd/goplay
+	go get github.com/mdempsky/gocode
+	go get github.com/sqs/goreturns
+	go get github.com/uudashr/gopkgs/v2/cmd/gopkgs
+	go get github.com/zmb3/gogetdoc
+	go get golang.org/x/lint/golint
+	go get golang.org/x/tools/cmd/gorename
+	go get golang.org/x/tools/gopls
+	go get github.com/cweill/gotests/...
+	go get github.com/rogpeppe/godef
+	go get github.com/ramya-rao-a/go-outline
+	go get github.com/go-delve/delve/cmd/dlv
 fi
