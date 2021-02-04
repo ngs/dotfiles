5efdddad80cbe4fd074acf893a19681c1a7df927
Atsushi Nagase <a@ngs.io>
2021-02-04 21:00:01 +0000

    Install mocp

--- setup.d/apt.sh
+++ setup.d/apt.sh
@@ -3,6 +3,16 @@ set -eux
 
 if command -v apt-get &> /dev/null; then
   sudo apt-get update -y
-  sudo apt-get install -y python3-dev tmux build-essential git vim zsh
+  sudo apt-get install -y \
+    python3-dev \
+    tmux \
+    build-essential \
+    git \
+    vim \
+    zsh \
+    moc \
+    moc-ffmpeg-plugin \
+    curl \
+    apt-transport-https
 fi
 
