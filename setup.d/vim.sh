f99de2ba8a5b69d5acff610536bd9934423a658a
Atsushi Nagase <a@ngs.io>
2021-02-09 05:16:36 +0900

    Use YouCompleteMe

--- setup.d/vim.sh
+++ setup.d/vim.sh
@@ -5,4 +5,9 @@ DOTFILES=$(cd $(dirname $0)/.. && pwd)
 
 curl -sfLo ${DOTFILES}/rc.d/vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +PlugInstall +qall > /dev/null 2>&1
+vim +PlugInstall +qall
+
+cd "${DOTFILES}/rc.d/vim/plugged/YouCompleteMe"
+python3 install.py --all
+
+
