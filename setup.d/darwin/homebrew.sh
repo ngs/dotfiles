a96f2c088be72109ba4892c2832c73cc99c58d5e
Atsushi Nagase <a@ngs.io>
2021-06-05 18:27:26 +0900

    Rum brew bundle with -vd

--- setup.d/darwin/homebrew.sh
+++ setup.d/darwin/homebrew.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 DOTFILES=$(cd $(dirname $0)/../.. && pwd)
+export ACCEPT_EULA='Y'
 ##
 [ `which brew` ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew tap Homebrew/bundle
-brew bundle --file="${DOTFILES}/Brewfiles/default"
-brew bundle --file="${DOTFILES}/Brewfiles/fonts"
-brew bundle --file="${DOTFILES}/Brewfiles/casks"
-brew bundle --file="${DOTFILES}/Brewfiles/mas"
+brew bundle --file="${DOTFILES}/Brewfiles/default" -vd
+brew bundle --file="${DOTFILES}/Brewfiles/fonts" -vd
+brew bundle --file="${DOTFILES}/Brewfiles/casks" -vd
+brew bundle --file="${DOTFILES}/Brewfiles/mas" -vd
 
