00f134b4c7753ef24497c7584189b68956ddb057
Atsushi Nagase <a@ngs.io>
2022-01-10 22:59:39 +0900

    Update

--- setup.d/darwin/theme.sh
+++ setup.d/darwin/theme.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 DOTFILES=$(cd $(dirname $0)/../.. && pwd)
-##
-## Import iTerm 2 Themes
 #
-
-for app in $(find /usr/local/Caskroom/iterm2 -name 'iTerm.app'); do
-  for f in $DOTFILES/themes/iterm2/*; do
-    THEME=$(basename "$f")
-    defaults write -app $app 'Custom Color Presets' -dict-add "$THEME" "$(cat "$f")"
-  done
-done
-
 [ -e ~/Dropbox/tomorrow-theme ] || /bin/sh -c 'git clone git@github.com:chriskempson/tomorrow-theme.git ~/Dropbox/tomorrow-theme'
 
 for f in ~/Dropbox/tomorrow-theme/OS\ X\ Terminal/*.terminal; do
