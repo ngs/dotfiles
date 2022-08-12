0f43eabca6cd3e45168b56ae524aee1cb75a7ac7
Atsushi Nagase <a@ngs.io>
2022-08-13 03:19:20 +0900

    Add eval

--- env.d/php.sh
+++ env.d/php.sh
@@ -3,4 +3,5 @@ PHPENV_ROOT="$HOME/.phpenv"
 if [ -d $PHPENV_ROOT ]; then
   export PHP_BUILD_CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-bz2=$(brew --prefix bzip2) --with-curl=$(brew --prefix curl) --with-iconv=$(brew --prefix libiconv) --with-libedit=$(brew --prefix libedit) --with-ssl=$(brew --prefix openssl@1.1)"
   export PATH="$PHPENV_ROOT/bin:$PATH"
+	eval "$(phpenv init -)"
 fi
