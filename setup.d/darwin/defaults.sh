4594c151c63b45448912e306c32cc805274eb7c4
Atsushi Nagase <a@ngs.io>
2023-03-26 04:06:57 +0900

    Update dotfiles

--- setup.d/darwin/defaults.sh
+++ setup.d/darwin/defaults.sh
@@ -6,4 +6,4 @@ defaults write -g KeyRepeat -int 1
 
 defaults write com.runningwithcrayons.Alfred-Preferences-3 syncfolder -string ~/Dropbox/Library/Alfred
 
-defaults write com.apple.Terminal 'Startup Window Settings' -string 'Tomorrow Night Eighties'
+# defaults write com.apple.Terminal 'Startup Window Settings' -string 'Tomorrow Night Eighties'
