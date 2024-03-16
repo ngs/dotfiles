aa2f24b72a1dc448a9c7df48cb3437db5556f8d9
Atsushi NAGASE <a@ngs.io>
2024-03-16 16:15:58 +0900

    Update for M3 MBAir

--- env.d/darwin/01_homebrew.sh
+++ env.d/darwin/01_homebrew.sh
@@ -1,1 +1,5 @@
-[ -e /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)" 
+if [ -e /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  LDFLAGS+=" -L$(brew --prefix)/lib "
+  export LDFLAGS
+fi
