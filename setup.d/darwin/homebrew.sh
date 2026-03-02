3e68992d2207aefa034de7360fe9a8ea73b4bd2f
Atsushi NAGASE <a@ngs.io>
2026-03-02 17:37:57 +0900

    chore: update Brewfiles, vim-plug, and Claude settings
    Update Homebrew cask/formula lists: add ChatGPT, Claude, and
    Claude Code; remove discontinued or unused packages. Use fully
    qualified formula names to fix deprecation warnings. Update
    vim-plug to latest version with shell_error handling changes.
    Remove deprecated `brew tap Homebrew/bundle` and add
    HOMEBREW_ACCEPT_EULA env var.

--- setup.d/darwin/homebrew.sh
+++ setup.d/darwin/homebrew.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 DOTFILES=$(cd $(dirname $0)/../.. && pwd)
 export ACCEPT_EULA='Y'
+export HOMEBREW_ACCEPT_EULA='Y'
 ##
 [ `which brew` ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
-brew tap Homebrew/bundle
 brew bundle --file="${DOTFILES}/Brewfiles/default" -vd
 brew bundle --file="${DOTFILES}/Brewfiles/fonts" -vd
 brew bundle --file="${DOTFILES}/Brewfiles/casks" -vd
